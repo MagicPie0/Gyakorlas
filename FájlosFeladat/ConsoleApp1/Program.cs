d9873c161f45b8a184b418c7cbba7b8448af0d99
Fülöp Krisztián <106276474+MagicPie0@users.noreply.github.com>
2025-04-14 19:48:51 +0200

    ElsoOra

---
+++ FájlosFeladat/ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kiolvassuk a szöveget
+            string szamokSzoveg = File.ReadAllText("szamok.txt");
+
+            //Beletesszük egy tömbbe
+            string[] szamok = szamokSzoveg.Split(',');
+            
+            
+            //A tömb körbe járásval ki irattatom a számokat
+            Console.Write("Rendezetlen számok: ");
+            foreach (string s in szamok)
+            {
+                Console.Write(s + ',');
+            }
+
+            //Számmá konvertálom a tömböt
+            int[] szam = new int[szamok.Length];
+
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                szam[i] = int.Parse(szamok[i]);
+               
+            }
+            
+            //Sorrenbe teszem a tömböt bubble sort-tal
+            /*for (int i = 0;i < szam.Length - 1; i++)
+            {
+                for (int j = 0; j < szam.Length - i - 1 ; j++)
+                {
+                    if (szam[i] > szam[j+1])
+                    {
+                        int ideiglenes = szam[j];
+                        szam[j] = szam[j + 1];
+                        szam[j + 1] = ideiglenes;
+                    }
+                }
+                break;
+            }*/
+
+            Array.Sort(szam);
+
+            //Ki írjuk a rendezetett
+            Console.Write("\nRendezett: ");
+            for (int i = 0; i < szam.Length; i++)
+            {
+                Console.Write(szam[i].ToString() + ',');
+            }
+
+            //Össze adjuk a számokat
+            int osszeg = 0;
+            for (int i = 0; i < szam.Length; i++)
+            {
+                osszeg += szam[i];
+            }
+
+            Console.WriteLine("\nÖsszeg: " + osszeg);
+
+        }
+    }
+}
---
+++ Gömbös feladat/ConsoleApp2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    //Kör számítása
+    public class CircleCalculator
+    {
+        //Kör kerülete
+        public double GetCircumference(double radius)
+        {
+            double kerulet = 2 * radius * Math.PI;
+            return kerulet;
+        }
+
+        //Kör területe
+        public double GetArea(double radius)
+        {
+            double terulet = Math.Pow(radius, 2) * Math.PI;
+            return terulet;
+        }
+    }
+
+    //Gömb számítása 
+    public class SphereCalculator
+    {
+        //A gömb térfogata
+        public double GetVolume(double radius)
+        {
+            double terfogat = (4 + Math.Pow(radius, 3) * Math.PI) / 3;
+            return terfogat;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Írd be a számítandó kör vagy gömb sugarát: ");
+            //Be kérjük a rádiuszt a felhasználótól
+            double radius = Console.Read();
+
+            CircleCalculator kor = new CircleCalculator();
+
+            Console.WriteLine("A kör kerülete: " + kor.GetCircumference(radius));
+            Console.WriteLine("A kör területe: " + kor.GetArea(radius));
+
+            SphereCalculator gomb = new SphereCalculator();
+            Console.WriteLine("A gömb térfogata: " + gomb.GetVolume(radius));
+        }
+    }
+}
