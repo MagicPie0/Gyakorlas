137271b013428152cd2c7b5126d9f4ba29e6af0e
Fülöp Krisztián <106276474+MagicPie0@users.noreply.github.com>
2025-04-16 23:41:56 +0200

    3.ora

---
+++ 3.óra/Kedvenc Gyumolcs/Kedvenc Gyumolcs/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Kedvenc_Gyumolcs
+{
+    public class Gyumolcs
+    {
+        public Gyumolcs(string nev, string szin)
+        {
+            this.nev = nev;
+            this.szin = szin;
+        }
+
+        public string nev {  get; set; }
+
+        public string szin { get; set; }
+    }
+
+    internal class Program
+    {
+        static List<Gyumolcs> gyumolcs = new List<Gyumolcs>(); 
+
+        static void ujGyumolcs()
+        {
+            Console.WriteLine("Írd be a nevét: ");
+            string nev = Console.ReadLine();
+            Console.WriteLine("Írd be a színét: ");
+            string szin = Console.ReadLine();
+
+            gyumolcs.Insert(0, new Gyumolcs(nev, szin));
+        }
+
+        static void gyumolcsokListazasa()
+        {
+            if (gyumolcs.Count > 0) 
+            {
+                Console.WriteLine(File.ReadAllText("gyumolcsok.txt") + '\n' + "Még nem lementett gyümölcs: " + gyumolcs[0].nev + ';' + gyumolcs[0].szin);
+            }
+            else
+            {
+                Console.WriteLine(File.ReadAllText("gyumolcsok.txt"));
+            }
+        }
+
+        static void mentes()
+        {
+            File.AppendAllText("gyumolcsok.txt", '\n' + gyumolcs[0].nev + ';' + gyumolcs[0].szin);
+        }
+
+        static void Main()
+        {
+            if (!File.Exists("gyumolcsok.txt"))
+            {
+                File.WriteAllText("gyumolcsok.txt", "Alma;piros");
+            }
+
+            Console.WriteLine("Új gyümölcs hozzá adása(1)");
+            Console.WriteLine("Gyümölcsök listázása(2)");
+            Console.WriteLine("Kilépés és mentés(3): ");
+            string cmd = Console.ReadLine();
+
+            switch (cmd) 
+            {
+                case "1":
+                    ujGyumolcs();
+                    Main();
+                    break;
+                case "2":
+                    gyumolcsokListazasa();
+                    Main();
+                    break;
+                case "3":
+                    mentes();
+                    return;
+                default:
+                    return;
+            }
+        }
+    }
+}
---
+++ 3.óra/Osztalyok_Rekurzio/Osztalyok/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osztalyok
+{
+    public class Teszt1
+    {
+        public string szin;
+        public int meret;
+
+        public virtual void Metodus1()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public int Metodus2() 
+        {
+            meret = 5;
+            return meret;
+        }
+    } 
+
+    public class Teszt2 : Teszt1
+    {
+        public void teszt2Metodus1()
+        {
+            szin = "szex";
+            Metodus1();
+        }
+    }
+
+    public class Teszt3 : Teszt1
+    {
+        public override void Metodus1()
+        {
+            Console.WriteLine("Szia");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Teszt1 teszt1 = new Teszt1();
+
+            Teszt1 teszt3 = new Teszt3();
+
+            teszt1.Metodus1();
+            teszt3.Metodus1();
+
+            Main();
+        }
+
+    }
+}
